02aab734c31c30dcf32dcb42a461a81c9c68fb19
unknown <adrianballester02@gmail.com>
2024-11-08 02:53:42 +0100

    Añadido Nav y HeroImage

---
+++ scripts/translations.js
@@ -0,0 +1,14 @@
+const translations = {
+    "es": {
+        "title": "Bienvenidos",
+        "description" : "Descripcion"
+    },
+
+    "en": {
+        "title": "Welcome to Mayayo Garden",
+        "description": "This is a trial for the description"
+    }
+};
+
+console.log(translations["en"]["title"]);
+
